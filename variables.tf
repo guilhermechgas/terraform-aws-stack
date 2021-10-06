8fdaf382c29fdbc2ea54eb17d481277de400d099
Guilherme Cordeiro <guilhermecordeiro2009@outlook.com.br>
2021-10-06 20:06:54 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,5 +25,5 @@ variable "instance_type" {
 
 variable "public_key" {
   description = "AWS EC2 Key Pair"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5CcaBb/BzlXR4sDhgIuitUX/BWAITvJKDk7HRlRDpL9huVcTrmagBK9aZRkvSM0bzvtXSRAksPwZLL5QUguio+UdgVmBBR04Vbou6qVw6J61F496Qn5zA+Kvda0vZ4XOsHFN/B6dO1w3pT4aeLotwGRRQ79FP4Kzrb5JohI5eljcNdhtmSHg5NkvYleHkWaNFu6+9fk5LVGpVwgF/1EFFr705aBi0VcbEUIc5YcE/cZXVlX8K5PfZ8rqcl1rHVvEqEv/gUCiVtBCeuGxPrkNPqk2HbjGM5oEaI2kJxwV2gtUHrqq5FNpSSYGkXB6nelJI8EhbI75P1z3N7/cT/kfH root@dxtcps010096"
+  default = "put_your_key_here"
 }
